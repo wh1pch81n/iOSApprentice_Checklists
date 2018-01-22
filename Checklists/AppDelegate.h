01ffd01dc2aa3005843419c15a3ec5b63e0978ee
Derrick Ho <derrickho@derricks-macbook-pro.local>
2018-01-21 17:27:57 -0800

    Initial Commit

---
+++ Checklists/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Checklists
+//
+//  Created by Derrick Ho on 1/21/18.
+//  Copyright © 2018 Derrick Ho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Checklists/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Checklists
+//
+//  Created by Derrick Ho on 1/21/18.
+//  Copyright © 2018 Derrick Ho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
